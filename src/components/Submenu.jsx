1dd04a149b9cdf2ea4da36ecf4dc10a1f9d03803
virghiu <virghiu@gmail.com>
2024-03-11 10:50:09 -0400

    feat: add links to submenu

--- src/components/Submenu.jsx
+++ src/components/Submenu.jsx
@@ -1,5 +1,32 @@
+import { useGlobalContext } from './../Context'
+import sublinks from './../data'
+
 const Submenu = () => {
-  return <div>Submenu</div>
+  const { pageId } = useGlobalContext()
+  const currentPage = sublinks.find((item) => item.pageId === pageId)
+
+  return (
+    <div className='submenu'>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className='submenu-links'
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr',
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { label, icon, url, id } = link
+          return (
+            <a href={url} key={id}>
+              {icon}
+              {label}
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Submenu
